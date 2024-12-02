0a6360b9266ad16b9a6f86e941fe0541f2f18b28
Iammujeeb45 <mujeeb.hardwin@gmail.com>
2024-12-02 19:12:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+let grandTotal = 0;
+let invoicegrandTotal = 0;
+let details = [];
+
+function litsenToClick(e) {
+  e.preventDefault();
+  const itemName = document.getElementById("itemName").value;
+  const itemPrice = parseFloat(document.getElementById("itemPrice").value);
+  if (!itemName || !itemPrice || itemPrice <= 0) {
+    alert("PLEASE ENTER VALID ITEMANME AND ITEMPRICE");
+    return;
+  }
+
+  const gst = itemPrice * 0.18;
+  const subTotal = itemPrice + gst;
+  grandTotal += subTotal;
+  invoicegrandTotal += subTotal;
+
+  const tableBody = document.querySelector("tbody");
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td class="px-6 py-4 text-black">${itemName}</td>
+    <td class="px-6 py-4 text-black">₹${itemPrice.toFixed(2)}</td>
+    <td class="px-6 py-4 text-black">₹${subTotal.toFixed(2)}</td>
+  `;
+  tableBody.appendChild(newRow);
+
+  document.getElementById("itemName").value = "";
+  document.getElementById("itemPrice").value = "";
+
+  document.getElementById(
+    "mTotal"
+  ).innerText = `GrandTotal : ₹${grandTotal.toFixed(2)}`;
+
+  details.push({ itemName, itemPrice, subTotal, grandTotal });
+  return;
+}
+document.getElementById("addItem").addEventListener("click", litsenToClick);
+
+function viewInvoice(e) {
+  e.preventDefault();
+
+  const tableBody = document.getElementById("invoiceDisplay");
+  tableBody.innerHTML = "";
+  invoiceDetails = details;
+
+  invoiceDetails.forEach((product, index) => {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+      <td class="px-6 py-4 text-black">${index + 1}</td>
+      <td class="px-6 py-4 text-black">${product.itemName}</td>
+      <td class="px-6 py-4 text-black">₹${product.itemPrice.toFixed(2)}</td>
+      <td class="px-6 py-4 text-black">₹${product.subTotal}</td>
+    `;
+    tableBody.appendChild(newRow);
+
+    document.getElementById(
+      "gTotal"
+    ).innerText = `Grand Total : ₹${product.grandTotal.toFixed(2)}`;
+  });
+}
+
+document.getElementById("viewInvoice").addEventListener("click", viewInvoice);
+
+function resetInvoice(e) {
+  e.preventDefault();
+
+  document.getElementById("itemName").value = "";
+  document.getElementById("itemPrice").value = "";
+  document.querySelector("tbody").innerHTML = "";
+  document.getElementById("mTotal").innerText = "Grand Total : ₹0.00";
+
+  document.getElementById("invoiceDisplay").innerHTML = "";
+
+  document.getElementById("gTotal").innerText = "Grand Total : ₹0.00";
+  details = [];
+  grandTotal = 0;
+  invoicegrandTotal = 0;
+  invoiceDetails = [];
+
+  myDiv.style.display = "none";
+}
+
+document.getElementById("resetBtn").addEventListener("click", resetInvoice);
+const myDiv = document.getElementById("myDiv");
+
+function viewBill() {
+  if (myDiv.style.display === "none" || myDiv.style.display === "") {
+    myDiv.style.display = "block";
+  } else {
+    myDiv.style.display = "none";
+  }
+  
+}
+document.getElementById("viewInvoice").addEventListener("click", viewBill);
